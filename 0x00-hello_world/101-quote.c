8a2d733d71609c024a2c128d847e51e770bec693
kiskiller0 <kiskiller0@gmial.com>
2023-09-07 19:48:24 +0100

    sdsd

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Betty wants to understand this
+ * Return: 0 the return value in case everything goes ok
+ */
+
+int main(void)
+{
+	char phrase1[] = "and that piece of art is useful\"";
+	char phrase2[] = "- Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "%s%s", phrase1, phrase2);
+
+	return (0);
+}
---
+++ 0x00-hello_world/build.sh
@@ -0,0 +1,2 @@
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 101-quote.c -o quote.o
+./quote.o
