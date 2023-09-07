5a05d52eff153d89828ae883ff9ba362ca89dd4a
kiskiller0 <kiskiller0@gmial.com>
2023-09-07 19:23:27 +0100

    sdsd

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Betty wants to understand this
+ * Return: 0 the return value in case everything goes ok
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
+/* Size of a char: 1 byte(s) */
+/* Size of an int: 4 byte(s) */
+/* Size of a long int: 4 byte(s) */
+/* Size of a long long int: 8 byte(s) */
+/* Size of a float: 4 byte(s) */
+/*  */
