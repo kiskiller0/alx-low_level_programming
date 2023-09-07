378bba252245a64e618dc51f927a0a461384641b
kiskiller0 <kiskiller0@gmial.com>
2023-09-07 19:15:23 +0100

    sdsds

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Betty wants to understand this
+ * Return: 0 the return value in case everything goes ok
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
