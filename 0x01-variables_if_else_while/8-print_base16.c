291e7ae89eeda768aaf32329724b5d2c534bbb08
kiskiller0 <kiskiller0@gmial.com>
2023-09-08 15:56:39 +0100

    sdsd

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - this is main, what do you expect betty ?
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	int a = '0';
+
+	while (a != '9' + 1)
+	{
+		putchar(a++);
+		if (a == '9' + 1)
+		{
+			a = 'a';
+			while (a != 'e' + 1)
+			{
+				putchar(a++);
+			}
+			a = '9' + 1;
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
