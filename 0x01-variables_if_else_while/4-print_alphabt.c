153733c89f62a39c74c8dc5cc42d15fd647efab0
kiskiller0 <kiskiller0@gmial.com>
2023-09-08 15:40:39 +0100

    dsdsdsd

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - this is main, what do you expect betty ?
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a != 'z' + 1)
+	{
+		if(a == 'q' || a == 'e')
+		{
+			a ++;
+			continue;
+		}
+
+		putchar(a++);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
