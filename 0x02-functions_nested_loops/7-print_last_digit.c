2784d0c4c1e40c92ea1a9cb2f31b4d0bc9bc7fe6
kiskiller0 <kiskiller0@gmail.com>
2023-09-12 21:03:00 +0100

    sdsd

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - return something ???
+ * Return: returns 1 or 0 (bool casted to int)
+ * @n: the character that the function takes
+ */
+
+int print_last_digit(int n)
+{
+	putchar(n % 10 + 48);
+}
