d74c2fa6d79955a44fd7acf9286a5b5af918e46b
kiskiller0 <kiskiller0@gmail.com>
2023-09-12 23:38:28 +0100

    sdd

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *times_table - return something ???
+ *Return: returns 1 or 0 (bool casted to int)
+ */
+void times_table(void)
+{
+	int a, b;
+
+    a = 0;
+    b = 0;
+
+    while (a != 10)
+    {
+		int order;
+		order = (a*b);
+
+		if (order > 10)
+		{
+			putchar(order/10 + 48);
+			putchar(order%10 + 48);
+		}
+		else
+		{
+			putchar(a*b + 48);
+		}
+		putchar(' ');
+		putchar(',');
+
+        b++;
+
+        if (b == 10)
+        {
+            b = 0;
+            a ++;
+        }
+    }
+}
