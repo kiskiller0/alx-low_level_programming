7f142f3391b444375a3bacf4fe38ba9bcc7bf514
kiskiller0 <kiskiller0@gmail.com>
2023-09-12 20:50:24 +0100

    sdsd

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - return something ???
+ * Return: returns 1 or 0 (bool casted to int)
+ * @n: the character that the function takes
+ */
+
+int _abs(int n)
+{
+	return (n < 0 ? -n : n);
+}
