99e4bd3b86f1e22561d9d22d8771408340749b78
kiskiller0 <kiskiller0@gmail.com>
2023-09-13 00:08:47 +0100

    sdsd

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *add - return something ???
+ *@n: sdsdsd
+ *@m: sdsdsdsd
+ *Return: returns 1 or 0 (bool casted to int)
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
