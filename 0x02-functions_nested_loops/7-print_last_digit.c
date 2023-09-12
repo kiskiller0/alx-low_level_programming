d4433ee05a07746deba182ba49059406049e4a03
kiskiller0 <kiskiller0@gmail.com>
2023-09-12 21:45:55 +0100

    sdsd

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,13 @@
 
 int print_last_digit(int n)
 {
+	
+
+	int last_digit;
+
 	if (n < 0)
 		n *= -1;
 		
-	int last_digit;
-
 	last_digit = n % 10;
 	_putchar(last_digit + 48);
 	return (last_digit);
