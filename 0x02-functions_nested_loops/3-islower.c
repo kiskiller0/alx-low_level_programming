7b038f2aa02205467a35c329dcde5c3c52c32988
kiskiller0 <kiskiller0@gmail.com>
2023-09-12 20:24:27 +0100

    sadsad

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - return something ???
+ * Return: returns 1 or 0 (bool casted to int)
+ * @c: the character that the function takes
+ */
+
+int _islower(int c)
+{
+	return (c < 'a');
+}
