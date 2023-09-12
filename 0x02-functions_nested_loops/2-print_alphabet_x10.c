5472c83cdfd6ee67236e68638f43504e1c84b698
kiskiller0 <kiskiller0@gmail.com>
2023-09-12 20:13:56 +0100

    sdsd :

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - return something ???
+ * Return - none of your business
+ */
+
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+	int i;
+	i = 0;
+
+	while (i++ < 10)
+	{
+		while (a != 'z' + 1)
+			_putchar(a++);
+
+		_putchar('\n');
+	}
+}
