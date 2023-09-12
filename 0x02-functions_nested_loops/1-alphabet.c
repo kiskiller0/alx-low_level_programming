5edf85a513f6b2597ec0e3ac8bf9e4d20f77cd97
kiskiller0 <kiskiller0@gmail.com>
2023-09-12 19:57:07 +0100

    sdsdsd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * 1-alphabet - asdasdsadsad
+ * print_alphabet: adsasdsadsad
+ */
+
+void print_alphabet()
+{
+	char a = 'a';
+
+	while (a != 'z' + 1)
+		_putchar(a++);
+
+	_putchar('\n');
+}
