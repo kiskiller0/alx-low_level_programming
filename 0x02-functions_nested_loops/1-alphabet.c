b99a3b27c9bc49c77ce43a943ce7594eaa2f6a1e
kiskiller0 <kiskiller0@gmail.com>
2023-09-12 20:11:40 +0100

    asdsd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,12 @@
 #include "main.h"
 
 /**
- * 1-alphabet - asdasdsadsad
  * print_alphabet - return something ???
+ * Return - none of your business
  */
 
 void print_alphabet(void)
 {
-
-	/**
-	 * print_alphabet - print all the letters of the alphabet
-	 */
-
 	char a = 'a';
 
 	while (a != 'z' + 1)
