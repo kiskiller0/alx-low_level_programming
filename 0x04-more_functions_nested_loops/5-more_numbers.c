fd5a8b25213b1df53d238b28cc22ed972a17f26c
kiskiller0 <kiskiller0@gmail.com>
2023-09-14 19:19:03 +0100

    sdsd

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@ void more_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 14; i++)
+	for (i = 0; i < 10; i++)
 	{
 		int a;
 
 		a = 0;
 
-		while (a++ < 15)
+		while (a < 15)
 		{
 			if (a < 10)
 				_putchar(a + 48);
@@ -24,6 +24,7 @@ void more_numbers(void)
 				_putchar(a / 10 + 48);
 				_putchar(a % 10 + 48);
 			}
+			a++;
 		}
 
 		_putchar('\n');
