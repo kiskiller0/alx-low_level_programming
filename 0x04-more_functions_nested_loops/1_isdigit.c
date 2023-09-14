52e0c4707c3ec77e2a18b8c7a0a252b5f913c841
kiskiller0 <kiskiller0@gmail.com>
2023-09-14 18:08:46 +0100

    fixed error

---
+++ 0x04-more_functions_nested_loops/1_isdigit.c
@@ -0,0 +1,12 @@
+
+#include "main.h"
+/**
+ * _isdigit - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
