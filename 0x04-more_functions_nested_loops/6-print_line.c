120963412e624eaf6e4eee8d61d3da91c13271f8
kiskiller0 <kiskiller0@gmail.com>
2023-09-14 21:52:30 +0100

    7

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+
+/**
+ *print_line - tiply
+ *@n: arg
+ *Return: a*b
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
+
