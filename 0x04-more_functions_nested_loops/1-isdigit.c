caf650a4f517ccd93f88964eb9ab4433e0c01f1c
kiskiller0 <kiskiller0@gmail.com>
2023-09-14 18:34:03 +0100

    magic

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,16 @@
-
 #include "main.h"
+
 /**
- * _isdigit - checks for uppercase character
- * @c: the character to be checked
- * Return: 1 if c is uppercase, 0 otherwise
+ * _isdigit - checks if c is digit
+ * @c: the arg
+ * Return: 1 in case true, 0 in case of false
  */
+
+
+
+
 int _isdigit(int c)
 {
-	return (c >= 0 && c <= 9);
+	return (c <= '9' && c >= '0');
 }
 
