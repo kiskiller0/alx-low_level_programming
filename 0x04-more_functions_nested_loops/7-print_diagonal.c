b82f74cd517b90b739415267d559d8e503a99a3e
kiskiller0 <kiskiller0@gmail.com>
2023-09-14 22:16:18 +0100

    sdsds

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,6 +12,12 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
 	for (i = 0; i < n; i++)
 	{
 		for (j = 1; j <= i; j++)
@@ -21,6 +27,5 @@ void print_diagonal(int n)
 		_putchar('\\');
 		_putchar('\n');
 	}
-
 }
 
