a96d818751e258575827f9acfc2be10fd307c316
kiskiller0 <kiskiller0@gmail.com>
2023-09-14 19:29:35 +0100

    sdsd

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,37 +1,25 @@
 #include "main.h"
 
 /**
- * more_numbers - tiply
- * Return: a*b
+ *more_numbers - tiply
+ *Return: a*b
  */
-void my_print(char c)
-{
-	_putchar(c);
-}
 
+/**
+ *my_print - lol
+ *@c: char c
+ * Return: void
+ */
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
-	{
-		int a;
-
-		a = 0;
-
-		while (a < 15)
+		for (j = 0; j < 15; j++)
 		{
-			if (a < 10)
-				my_print(a + 48);
-			else
-			{
-				my_print(a / 10 + 48);
-				my_print(a % 10 + 48);
-			}
-			a++;
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
-
-		my_print('\n');
-	}
+		_putchar('\n');
 }
-
