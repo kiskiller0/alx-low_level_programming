8dd68f91a0002ae4021e77530824aa033df9720b
kiskiller0 <kiskiller0@gmail.com>
2023-09-14 22:30:02 +0100

    sdsd

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ *print_square - tiply
+ *@size: arg
+ *Return: a*b
+ */
+
+int main(void)
+{
+	int i;
+	int is_fb;
+
+	for (i = 1; i < 101; i++)
+	{
+		is_fb = 0;
+
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+			is_fb = 1;
+		}
+		if (i % 5 == 0)
+		{
+			is_fb = 1;
+			printf("Buzz");
+		}
+
+		if (!is_fb)
+			printf("%d", i);
+
+		putchar(' ');
+	}
+}
+
