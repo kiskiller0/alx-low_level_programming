02e300bf2a2ff273bf83eb661cb9fa6e3926accb
kiskiller0 <kiskiller0@gmail.com>
2023-09-14 14:10:03 +0100

    sdsd

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,21 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- *main - tests function that prints if integer is positive or negative
- *Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 98;
-	int result;
+        i = 98;
+        positive_or_negative(i);
 
-	result = positive_or_negative(i);
-	if (result == 0)
-		printf("0 is zero");
-
-	return (0);
+        return (0);
 }
