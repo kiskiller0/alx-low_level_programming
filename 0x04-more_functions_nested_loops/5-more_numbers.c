5f399ee47344c3eeae5ce8a2ad28989d3011c26f
kiskiller0 <kiskiller0@gmail.com>
2023-09-14 19:13:31 +0100

    asdasd

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - tiply
+ * Return: a*b
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 14; i++)
+	{
+		int a;
+
+		a = 0;
+
+		while (a < 15)
+		{
+			if (a < 10)
+				_putchar(a + 48);
+			else
+			{
+				_putchar(a / 10 + 48);
+				_putchar(a % 10 + 48);
+			}
+		}
+
+		_putchar('\n');
+}
+
