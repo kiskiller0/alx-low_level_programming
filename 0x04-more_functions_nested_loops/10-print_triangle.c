af1b90bc4e7e9bd5acffd023d29b865f1426479a
kiskiller0 <kiskiller0@gmail.com>
2023-09-14 22:53:39 +0100

    sdsd

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *print_triangle - tiply
+ *@size: sadasd
+ *Return: a*b
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	int i, j, k;
+
+	for (i = 1; i <= size; i++)
+	{
+
+		for (j = 0; j < size - i; j++)
+		{
+			_putchar(' ');
+		}
+
+		for (k = 0; k < i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
+
