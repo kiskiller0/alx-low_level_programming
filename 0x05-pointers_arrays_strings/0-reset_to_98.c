ce463fe15d3fd98e59a77151c3051e78b45030f3
kiskiller0 <kiskiller0@gmail.com>
2023-09-18 19:00:47 +0100

    sdsd

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - change a number through pass by address
+ * @n: the number to change
+ * Return: void
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
+
