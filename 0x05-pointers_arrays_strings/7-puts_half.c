47ab7728585d763d68dddd8268514a4cee56847e
kiskiller0 <kiskiller0@gmail.com>
2023-09-18 20:31:46 +0100

    again, accounting for 0-based

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -24,7 +24,7 @@ void puts_half(char *str)
 	if (strlen % 2 == 0)
 		i = strlen / 2 - 1;
 	else
-		i = strlen - ((strlen - 1) / 2);
+		i = strlen - ((strlen - 1) / 2) - 1;
 	str--;
 
 	while (i != strlen)
@@ -32,5 +32,7 @@ void puts_half(char *str)
 		i++;
 		_putchar(head[i]);
 	}
+
+	_putchar('\n');
 }
 
