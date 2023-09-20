4a69acbf355ac37f32d17e6528e895ba4b4e3eb2
kiskiller0 <kiskiller0@gmail.com>
2023-09-20 19:58:59 +0100

    1 -> 15

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,15 +15,15 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] > s2[i])
-			return (1);
+			return (15);
 		else if (s1[i] < s2[i])
-			return (-1);
+			return (-15);
 	}
 
 	if (s1[i] != '\0')
-		return (1);
+		return (15);
 	else if (s2[i] != '\0')
-		return (-1);
+		return (-15);
 	else
 		return (0);
 }
