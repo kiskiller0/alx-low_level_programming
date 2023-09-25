f13efd7ae1ca3518d5e3c01581708f26ed9636fd
kiskiller0 <kiskiller0@gmail.com>
2023-09-25 17:31:37 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,30 @@
+
+
+#include "main.h"
+
+/**
+ * _memcpy - extract number from withing string, with its sign
+ *@dest: the dest
+ *@src: the source
+ *@n: size to fill
+ *Return: the memory location of s
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *head;
+	unsigned int i;
+
+	head = dest;
+
+
+	for (i = 0; i < n; i++)
+	{
+		*dest =  *src;
+		s++;
+		src++;
+	}
+
+	return (head);
+}
+
