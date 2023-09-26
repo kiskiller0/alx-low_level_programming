e77cfce76aa8c1ac248fa7ae0d87afff1ebbc8f2
kiskiller0 <kiskiller0@gmail.com>
2023-09-26 01:22:12 +0000

    weird

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *set_string - copy one string to another or sth
+ * @s: the src
+ * @to: the dst
+ * Return: nada!
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
--- 0x07-pointers_arrays_strings/build.sh
+++ 0x07-pointers_arrays_strings/build.sh
@@ -1,3 +1,3 @@
 clear
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 8-main.c 8-print_diagsums.c -o 8-print_diagsums
-./8-print_diagsums
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-main.c 100-set_string.c -o 100-set_string
+./100-set_string 
