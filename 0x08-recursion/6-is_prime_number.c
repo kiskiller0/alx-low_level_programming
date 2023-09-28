9acdd7a65806406bfb2f8970f5452238dd70e17d
kiskiller0 <kiskiller0@gmail.com>
2023-09-28 18:41:46 +0100

    sdsd

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * isprime - check if prime
+ * @n: the number
+ * @i: the first possible dividant
+ * Return: nada!
+ */
+int isprime(int n, int i)
+{
+	if (n % i == 0)
+	{
+		return (-1);
+	}
+	if (i == 1)
+	{
+		return (1);
+	}
+
+	isprime(n, --i);
+}
+/**
+ * is_prime_number - check if a number is prime
+ * @n: the number
+ * Return: nada!
+ */
+
+int is_prime_number(int n)
+{
+	return (isprime(n, n - 1));
+}
+
