f9bc78dbc68d79326ecbe0d69cc37eb07e80a89c
kiskiller0 <kiskiller0@gmail.com>
2023-09-28 18:35:39 +0100

    trickery

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,35 @@
-
-
 #include "main.h"
 #include <stdio.h>
 
 /**
- * *_sqrt_recursion - find the square root of a natural number
+ * square - find the square root of a natural number
+ * @n: the number
+ * @i: the number
+ * Return: nada!
+ */
+int square(int n, int i)
+{
+	if (i == n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+	{
+		return (-1);
+	}
+
+	return (square(n, ++i));
+}
+/**
+ * _sqrt_recursion - find the square root of a natural number
  * @n: the number
  * Return: nada!
  */
+
 int _sqrt_recursion(int n)
 {
-	int root;
-
-	root = 1;
-	while (root != n)
-	{
-		if (root * root == n)
-			return (root);
-		root++;
-	}
-
-	return (-1);
+	return (square(n, 1));
 }
+
+
 
