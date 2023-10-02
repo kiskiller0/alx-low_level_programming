053bdb3b4b848dc18239f47187cc4eaa5a1e235d
kiskiller0 <kiskiller0@gmail.com>
2023-10-02 19:49:06 +0100

    sdsd

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print program name + \n
+ * @argc: arguments count
+ * @argv: arguments
+ * Return: 0 for success, otherwise for failure
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
