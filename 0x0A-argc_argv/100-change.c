f95dd4bc8d4d0e7d4bee1216736f74ec42d31db2
kiskiller0 <kiskiller0@gmail.com>
2023-10-03 00:27:08 +0100

    sdsd

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,52 +1,40 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - asdasdsa
- * @argc: sdsd
- * @argv: asdsad
- * Return: sdsdsd
+ *main - asdsad
+ *@argc: asdsad
+ *@argv: asdasdsadarray that contains the arguments.
+ *Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-	int j, num, money;
-	int change[] = {25, 10, 5, 2, 1};
+	int cents, ncoins = 0;
 
-	j = 0;
-	num = 0;
-	money = strtol(argv[1], 0, 10);
-
-
-	if (argc != 2)
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	if (money <= 0)
+	cents = atoi(argv[1]);
 
+	while (cents > 0)
 	{
-		printf("0\n");
-		return (0);
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
 	}
 
-	while (money)
-	{
-		if (change[j] > money)
-		{
-			j++;
-		}
-		else
-		{
-			money -= change[j];
-			num++;
-		}
-	}
-
-	printf("%d\n", num);
-
+	printf("%d\n", ncoins);
 	return (0);
 }
 
