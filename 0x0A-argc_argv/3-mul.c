b4d0f16574c7197213c2526eb43946d6807a5913
kiskiller0 <kiskiller0@gmail.com>
2023-10-02 20:50:31 +0100

    sdsdd

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,29 @@
+
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- *main - print program name + \n
- *@argc: arguments count
- *@argv: arguments
- *Return: 0 for success, otherwise for failure
+ * main - asdasdsa
+ * @argc: sdsd
+ * @argv: asdsad
+ * Return: sdsdsd
  */
+
 int main(int argc, char *argv[])
 {
+	long a;
+	long b;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	printf("%d\n", (*argv[1] - '0') * (*argv[2] - '0'));
+	a = strtol(argv[1], 0, 10);
+	b = strtol(argv[2], 0, 10);
+
+	printf("%ld\n", a * b);
 	return (0);
 }
+
