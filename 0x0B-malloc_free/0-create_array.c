b69c0e46ac998725a197e03291637a2fcb9f3496
kiskiller0 <kiskiller0@gmail.com>
2023-10-03 18:05:16 +0100

    sdsd

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+
+/**
+ * create_array - asdasdsad
+ * @size: ...
+ * @c: the char to initialize the array to
+ * Return: asdasdsad
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i;
+
+	arr = (char *) malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+
+
+	return (arr);
+}
+
