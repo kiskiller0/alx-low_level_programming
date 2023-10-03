bc0392af277c1a2739a26525a972e02923b79793
kiskiller0 <kiskiller0@gmail.com>
2023-10-03 19:27:02 +0100

    sdsd

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,45 @@
+
+#include "main.h"
+#include <stdlib.h>
+
+
+
+/**
+ * _strdup - cp a string to a new location n return a pointer
+ * @str: the string to copy
+ * Return: a ptr
+ */
+
+char *_strdup(char *str)
+{
+	char *arr;
+	unsigned int size_str;
+	char *head;
+
+	head = str;
+	size_str = 0;
+
+	while (head)
+	{
+		size_str++;
+		head++;
+	}
+
+	arr = (char *) malloc(sizeof(char) size_star + 1);
+	head = arr;
+
+	if (!arr)
+		return (arr);
+
+	while (*str)
+	{
+		*head = *str;
+		str++;
+		head++;
+	}
+
+	*head = *str;
+
+	return (arr);
+}
+
