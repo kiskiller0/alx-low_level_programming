8d073f5afa6e38f1975e95eca6a8df482e912e12
kiskiller0 <kiskiller0@gmail.com>
2023-10-09 23:41:06 +0100

    sdsd

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * array_range - malloc stuff
+ * array_range - sdsd
  * @min: s1
  * @max: s2
  * Return: a pointer to the newly allocated memory
@@ -15,10 +15,13 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
+
 	len = max - min + 1;
 	ptr = malloc(sizeof(int) * len);
+
 	if (!ptr)
 		return (NULL);
+
 	for (i = 0; i < len; i++)
 		ptr[i] = min++;
 	return (ptr);
