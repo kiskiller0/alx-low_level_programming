c617989da14865dacb7803403f4e6d420d28dc66
kiskiller0 <kiskiller0@gmail.com>
2023-10-09 22:07:29 +0100

    sdsd

--- 0x0C-more_malloc_free/build.sh
+++ 0x0C-more_malloc_free/build.sh
@@ -1,3 +1,3 @@
 clear
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-array_range.c -o 3-array_range
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-array_range.c -o 3-array_range -g
 ./3-array_range
