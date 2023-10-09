8df268b0ef2c5309bb4fa19c1072b24fbf774396
kiskiller0 <kiskiller0@gmail.com>
2023-10-09 19:17:18 +0100

    sdds

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range - malloc stuff
+ * @min: s1
+ * @max: s2
+ * Return: a pointer to the newly allocated memory
+ */
+
+int *array_range(int min, int max)
+{
+	int *arr, i;
+
+	if (min >= max)
+		return (NULL);
+
+	arr = malloc(sizeof(int) * (max - min));
+
+	if (!arr)
+		return (NULL);
+
+	for (i = min; i < max + 1; i++)
+		arr[i - min] = i;
+
+	return (arr);
+}
+
