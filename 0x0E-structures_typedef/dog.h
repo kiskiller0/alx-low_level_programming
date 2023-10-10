06aaddd2383fb023ebfc42e9c68195f34707601e
kiskiller0 <kiskiller0@gmail.com>
2023-10-11 00:41:23 +0100

    sdsd

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - dog
+ * @name: the name of the dog
+ * @age: asdasda
+ * @owner: sadsdsd
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+}
+
+
+#endif
