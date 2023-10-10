24fe5aa3072ef1d0be06012a36da61624f5bde0a
kiskiller0 <kiskiller0@gmail.com>
2023-10-11 00:54:28 +0100

    sdsd

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "dog.h"
 /**
  * init_dog - asdsad
  * @d: asdd
@@ -10,9 +10,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
 }
-
