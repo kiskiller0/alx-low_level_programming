1dde704ae862d6e0ac7aa59e853b02a35317d110
kiskiller0 <kiskiller0@gmail.com>
2023-10-11 00:46:04 +0100

    sdsd

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * init_dog - asdsad
+ * @d: asdd
+ * @name: asdsad
+ * @owner: sdd
+ * @age: sdd
+ */
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
