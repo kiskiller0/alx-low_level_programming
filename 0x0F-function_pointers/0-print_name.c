56dbee5dc051bcabc2a063d0be0365b7edb3a626
kiskiller0 <kiskiller0@gmail.com>
2023-10-11 18:50:55 +0100

    sdds

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_name - malloc stuff
+ * @name: s1
+ * @f: s2
+ * Return: a pointer to the newly allocated memory
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
