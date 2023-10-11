72a519af1ec4dd540a7e05457e029affb0c37c81
kiskiller0 <kiskiller0@gmail.com>
2023-10-11 01:10:25 +0100

    sdsd

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,16 +14,8 @@ void print_dog(struct dog *d)
 		return;
 	}
 
-	if (d->name)
-		printf("Name : %s\n", d->name);
-	else
-		printf("Name : (nil)\n");
-
-	printf("Age : %f\n", d->age);
-
-	if (d->owner)
-		printf("Owner : %s\n", d->owner);
-	else
-		printf("Owner : (nil)\n");
+	d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
+	printf("Age: %f\n", d->age);
+	d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
 
 }
