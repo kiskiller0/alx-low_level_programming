a52b99ada8d94808d4c4ae76c18f9d00775bcc89
kiskiller0 <kiskiller0@gmail.com>
2023-10-11 01:15:33 +0100

    sdsd

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,29 @@
+
+
+#include "dog.h"
+#include <stdio.h>
+/**
+ * new_dog - asdsad
+ * @age: asdd
+ * @name: sdsd
+ * @owner: asdd
+ * Return: sdsd
+ */
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+	dog_t *scooby = malloc(sizeof(dog_t));
+
+	if (!dog_t)
+		return (NULL);
+
+	dog_t->name = name;
+	dog_t->age = age;
+	dog_t->owner = owner;
+
+
+	return (scooby);
+}
+
