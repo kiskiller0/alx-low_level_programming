6bad885b1f843be5898973e578d4b413a68ac6fd
kiskiller0 <kiskiller0@gmail.com>
2023-10-20 01:36:29 +0100

    print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * print_list - prints all nodes in a llist
+ * @h: the head
+ * Return: the size(number of nodes)
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num;
+
+	for (num = 0; h; h = h->next)
+	{
+		if (h->str)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+
+		num++;
+	}
+
+	return (num);
+}
---
+++ 0x12-singly_linked_lists/build.sh
@@ -0,0 +1,3 @@
+clear
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-print_list.c -o a
+./a 
