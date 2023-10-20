983bf00c687a60b94bf7f696935721610a636a7b
kiskiller0 <kiskiller0@gmail.com>
2023-10-20 11:42:11 +0100

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * free_list - prints all nodes in a llist
+ * @head: the head
+ */
+
+void free_list(list_t *head)
+{
+
+	if (head->next)
+	{
+		free_list(head->next);
+		free(head);
+	} else
+		free(head);
+
+}
