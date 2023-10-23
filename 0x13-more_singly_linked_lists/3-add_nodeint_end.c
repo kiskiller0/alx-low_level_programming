48a594345403499b824465baefd3da9d1e0dba36
kiskiller0 <kiskiller0@gmail.com>
2023-10-23 21:18:02 +0100

    asdsd

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * add_nodeint_end - asdasd
@@ -13,24 +14,24 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
 
-	if (!*head || !*head->next)
+	if (!*head || !(*head)->next)
 	{
-		new = malloc(sizeof(listint));
+		new = malloc(sizeof(listint_t));
 
 		if (!new)
 			return (NULL);
 
-		new->n;
-		new->next = *head;
+		new->n = n;
+		new->next = NULL;
 		if (!*head)
 			*head = new;
 		else
-			*head->next = new;
+			(*head)->next = new;
 
 		return (new);
-	} 
+	}
 	else
 	{
-		return (&(*head->next), n);
+		return (add_nodeint_end(&((*head)->next), n));
 	}
 }
---
+++ 0x13-more_singly_linked_lists/build3.sh
@@ -0,0 +1,4 @@
+clear
+gcc -Wall -pedantic -Werror -Wextra -std=c99 3-main.c 3-add_nodeint_end.c 0-print_listint.c -o d -g
+./d
+# gdb d
