f3f5ecb640a6690a2a81563f546857264c278231
kiskiller0 <kiskiller0@gmail.com>
2023-10-23 21:50:37 +0100

    (nil)

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,5 +13,6 @@ void free_listint2(listint_t **head)
 	{
 		free_listint2(&(*head)->next);
 		free(*head);
+		*head = NULL;
 	}
 }
---
+++ 0x13-more_singly_linked_lists/build5.sh
@@ -0,0 +1,4 @@
+clear
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-main.c 3-add_nodeint_end.c 0-print_listint.c 5-free_listint2.c -o f
+./f 
+valgrind ./f
