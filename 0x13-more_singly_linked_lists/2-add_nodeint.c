768b4769c5927a5ec211b80700143ad99a32b24c
kiskiller0 <kiskiller0@gmail.com>
2023-10-23 15:59:17 +0100

    3

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add_nodeint - asdasd
+ * @head: the head of the nodes
+ * @n: the n field of the newly created node
+ * Return: the number of nodes
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint));
+
+	if (!new || !head)
+		return (NULL);
+
+	new->n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
