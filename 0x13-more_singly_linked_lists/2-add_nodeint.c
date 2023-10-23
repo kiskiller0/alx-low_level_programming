19cff86197e7747408319c0e2ffd647c56735e26
kiskiller0 <kiskiller0@gmail.com>
2023-10-23 19:49:42 +0100

    2:2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,12 +12,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
-	new = malloc(sizeof(listint));
+	new = malloc(sizeof(listint_t));
 
 	if (!new || !head)
 		return (NULL);
 
-	new->n;
+	new->n = n;
 	new->next = *head;
 	*head = new;
 
---
+++ 0x13-more_singly_linked_lists/build2.sh
@@ -0,0 +1,3 @@
+clear
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-add_nodeint.c 0-print_listint.c -o c
+./c
