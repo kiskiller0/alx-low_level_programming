e0967948104fc711770eba4f9b8bf2f000f0b59e
kiskiller0 <kiskiller0@gmail.com>
2023-10-28 03:08:43 +0100

    3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * set_bit - turn bin number to an unsigned int
+ * @n: the number in string form
+ * @index: the number in string form
+ * Return: nothing
+ */
+
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	return (!!(*n |= 1L index));
+}
