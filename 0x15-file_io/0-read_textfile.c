a739a7d130ff7c1408f8cb4fc15465ad9d626d57
kiskiller0 <kiskiller0@gmail.com>
2023-11-02 18:44:12 +0100

    Revert "fuck"
    This reverts commit 527823b85ba3850e469744a151b292c211d3a4dd.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,9 +9,9 @@
 
 size_t read_textfile(const char *filename, size_t letters)
 {
-	size_t b;
-	int file_desc;
-	char buffer[READ_BUF_SIZE * 8];
+	FILE *f = fopen(filename, "r");
+	char c;
+	size_t count;
 
 	if (!f || !letters)
 		return (0);
